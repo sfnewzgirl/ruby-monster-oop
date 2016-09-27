392d70d7ded958e37a70884f5a2d8653b2771e4e
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-27 14:00:54 -0700

    monster oop code from sam and me

--- starter_code/monster.rb
+++ starter_code/monster.rb
@@ -2,11 +2,21 @@ class Monster
   attr_accessor :habitat, :threat_level
   @@count = 0
   Threat_level = [:low, :medium, :high, :midnight]
+
+  def self.fight(attacker, defender)
+    if Threat_level.index(attacker.threat_level) > Threat_level.index(defender.threat_level)
+      'attacker wins!'
+    else
+      'defender wins!'
+    end
+  end
+
   def initialize(threat_level=:medium)
     @habitat = 'cave'
     @threat_level = threat_level
     puts "RAWR!"
     @@count += 1
+    puts "there are #{@@count} monsters"
     if !(Threat_level.include? (@threat_level))
       raise 'INVALID THREAT LEVEL'
     end
@@ -18,47 +28,42 @@ class Monster
   def self.count
     @@count
   end
+
+  def get_dangerous
+    # ruby version of a switch statement is case
+    case @threat_level
+      when :low
+        @threat_level = :medium
+      when :medium
+        @threat_level = :high
+      when :high
+        @threat_level = :midnight
+      when :midnight
+        :midnight
+    end
+  end
 end
 
+class Zombie < Monster
+  def initialize(threat_level=:medium)
+    super
+    @habitat = 'graveyard'
+  end
+end
 
-  # getters and setters for instance variables
+class Werewolf < Monster
+  def initialize(threat_level=:medium)
+    super
+    @threat_level = :low
+  end
+end
 
-  # class variable for count
+module Flying
+  def Fly
+  puts "it soars across the sky"
+  end
+end
 
-  # class instance variable for class description
-
-  # class method getter for @@count class variable
-
-  # class method getter for @class_description class instance variable
-
-  # initial behavior
-
-  # habitat? instance method
-
-  # get_dangerous instance method
-
-  # fight class method
-
-# Zombie class
-
-  # zombie version of class_description
-  
-  # initial behavior and values
-
-# Werewolf class
-
-  # werewolf version of class_description
-
-  # initial behavior and values
-
-  # update_threat_level instance method
-
-# Flying module
-
-  #fly method
-
-# Vampire class
-
-  # vampire class description
-
-  # Flying module included
+class Vampire < Monster
+  include Flying
+end
